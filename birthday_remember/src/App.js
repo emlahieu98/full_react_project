5f673c439a659e990b9211d1a1b8e218ec685f9f
emlahieu98 <hieucr98tb@gmail.com>
2021-04-01 15:07:10 +0700

    add pro

---
+++ birthday_remember/src/App.js
@@ -0,0 +1,19 @@
+
+import React , {useState} from 'react'
+import './App.css';
+import data from './data'
+import List from './List'
+function App() {
+  const [people,setPeople] = useState(data)
+  return (
+    <main>
+      <div className="container">
+        <h3>{people.length} birthday today </h3>
+        <List people={people} />
+        <button onClick={() => setPeople([])}>Clear All</button>
+      </div>
+    </main>
+  );
+}
+
+export default App;
---
+++ birthday_remember/src/data.js
@@ -0,0 +1,44 @@
+export default [
+  {
+    id: 1,
+    name: "Bertie Yates",
+    age: 29,
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg",
+  },
+  {
+    id: 10,
+    name: "David James",
+    age: 35,
+    image:
+      "https://ephoto360.com/uploads/worigin/2020/03/23/tao-avatar-mac-dinh-facebook-thay-nen-cuc-hot5e7838ae39057_96eb8aef68a3aa00523448390b49fbcb.jpg",
+  },
+  {
+    id: 2,
+    name: "Hester Hogan",
+    age: 32,
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-3_rxtqvi.jpg",
+  },
+  {
+    id: 3,
+    name: "Larry Little",
+    age: 36,
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg",
+  },
+  {
+    id: 4,
+    name: "Sean Walsh",
+    age: 34,
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg",
+  },
+  {
+    id: 5,
+    name: "Lola Gardner",
+    age: 29,
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg",
+  },
+];
