d17c49e94989f61ad40f417a34110e5cdcabdd81
emlahieu98 <hieucr98tb@gmail.com>
2021-04-05 15:09:34 +0700

    procject 4

---
+++ accordion/src/App.js
@@ -0,0 +1,24 @@
+import React , {useState} from 'react'
+import './App.css';
+import data from './data'
+import Question from './Question'
+function App() {
+  const [questions, setQuestions] = useState(data)
+  return (
+    <main>
+      <div className="container">
+        <h3>Questions and answers about login</h3>
+        <section className="info">
+          {         
+            questions.map((question) => {
+              return <Question key={question.id} {...question} />
+            }) 
+          }
+
+        </section>
+      </div>
+    </main>
+  );
+}
+
+export default App;
