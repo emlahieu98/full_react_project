8fce13b10d6468682750c25ddefaaaa1ac377a55
emlahieu98 <hieucr98tb@gmail.com>
2021-04-20 17:02:18 +0700

    update

--- 10-grocery-bud/src/List.js
+++ 10-grocery-bud/src/List.js
@@ -1,9 +1,34 @@
 import React from 'react'
 import {FaEdit, FaTrash} from 'react-icons/fa'
-const List = () => {
+const List = ({ items, removeItem, editItem }) => {
   return (
-    <h2>List component</h2>
-  )
-}
+    <div className="grocery-list">
+      {items.map((item) => {
+        const { id, title } = item;
+        return (
+          <article key={id} className="grocery-item">
+            <p className="title">{title}</p>
+            <div className="btn-container">
+              <button
+                onClick={() => editItem(id)}
+                type="button"
+                className="edit-btn"
+              >
+                <FaEdit />
+              </button>
+              <button
+                type="button"
+                className="delete-btn"
+                onClick={() => removeItem(id)}
+              >
+                <FaTrash />
+              </button>
+            </div>
+          </article>
+        );
+      })}
+    </div>
+  );
+};
 
 export default List
